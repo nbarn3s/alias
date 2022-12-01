a29a17b3cde29b92c17d9d7719be87bc3cbdc762
Nick Barnes <nickbarnes169@gmail.com>
2022-12-01 11:32:17 -0500

    add scraping tool

---
+++ scripts/readme.md
@@ -0,0 +1,6 @@
+# Supporting Scripts for Alias
+
+The scripts are focused on scraping the name data.  These are only needed for updating the data.
+The required packages can be installed with:
+
+    pip install -r requirements.txt
---
+++ scripts/scrapeUsaFornames.py
@@ -0,0 +1,46 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+
+
+def getFornameDecade(url):
+    options = webdriver.ChromeOptions()
+    options.headless = True
+    with webdriver.Chrome(
+        service=ChromeService(ChromeDriverManager().install()), options=options
+    ) as driver:
+        driver.get(url)
+
+        print("Scraping:", driver.current_url)
+        table = driver.find_element(By.CLASS_NAME, "t-stripe")
+        rows = table.find_elements(By.TAG_NAME, "tr")
+        tableData = []
+        for row in rows:
+            cells = row.find_elements(By.TAG_NAME, "td")
+            rowData = []
+            for cell in cells:
+                rowData.append(cell.text)
+            if len(rowData) > 1 and rowData[0].isdigit():
+                tableData.append(rowData)
+    return tableData
+
+
+def writeFornameFile(year):
+    url = f"https://www.ssa.gov/OACT/babynames/decades/names{year}s.html"
+    fileName = f"forenames_{year}s.csv"
+    header = "rank,maleName,number,femaleName,number"
+    data = getFornameDecade(url)
+    with open(fileName, "w", encoding="UTF8") as f:
+        f.write(f"{url}\n")
+        f.write(f"{header}\n")
+        for row in data:
+            row[0] = int(row[0])
+            row[2] = int(row[2].replace(",", ""))
+            row[4] = int(row[4].replace(",", ""))
+            f.write(f"{row[0]},{row[1]},{row[2]},{row[3]},{row[4]}\n")
+
+
+for year in range(1880, 2020, 10):
+    writeFornameFile(year)
